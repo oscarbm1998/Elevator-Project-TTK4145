2b8ba53300abca5ae0f86228aa564df48b7d21fd
Victor <victor.melhuus@gmail.com>
2022-02-18 13:51:10 +0100

    goTo

--- Driver-go/main.go
+++ Driver-go/main.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 )
 
+var current_floor int
+
 func main() {
 
 	numFloors := 4
 
 	elevio.Init("localhost:15657", numFloors)
 
-	var d elevio.MotorDirection = elevio.MD_Up
+	//var d elevio.MotorDirection = elevio.MD_Up
 	//elevio.SetMotorDirection(d)
 
 	drv_buttons := make(chan elevio.ButtonEvent)
@@ -32,21 +34,26 @@ func main() {
 
 		case a := <-drv_floors:
 			fmt.Printf("%+v\n", a)
-			if a == numFloors-1 {
-				d = elevio.MD_Down
-			} else if a == 0 {
-				d = elevio.MD_Up
-			}
-			elevio.SetMotorDirection(d)
+			/*
+				if a == numFloors-1 {
+					d = elevio.MD_Down
+				} else if a == 0 {
+					d = elevio.MD_Up
+				}
+			*/
+
+			//elevio.SetMotorDirection(d)
+			current_floor = a
 
 		case a := <-drv_obstr:
 			fmt.Printf("%+v\n", a)
-			if a {
-				elevio.SetMotorDirection(elevio.MD_Stop)
-			} else {
-				elevio.SetMotorDirection(d)
-			}
-
+			/*
+				if a {
+					elevio.SetMotorDirection(elevio.MD_Stop)
+				} else {
+					elevio.SetMotorDirection(d)
+				}
+			*/
 		case a := <-drv_stop:
 			fmt.Printf("%+v\n", a)
 			for f := 0; f < numFloors; f++ {
@@ -55,5 +62,17 @@ func main() {
 				}
 			}
 		}
+		go elevator_goTo(2)
+	}
+
+}
+
+func elevator_goTo(iDestination int) {
+	if current_floor < iDestination {
+		elevio.SetMotorDirection(elevio.MD_Up)
+	} else if current_floor > iDestination {
+		elevio.SetMotorDirection(elevio.MD_Down)
+	} else {
+		elevio.SetMotorDirection(elevio.MD_Stop)
 	}
 }
