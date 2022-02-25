47e5d278b36de52d71288e413ea9bb23a164533a
Taro-tkort <joel.morlin@gmail.com>
2022-02-25 14:57:53 +0100

    updata

--- SingleElevator/qeue.go
+++ SingleElevator/qeue.go
@@ -1,14 +1,35 @@
 package singleElevator
-import(
-	"fmt"
-	"main"
-)
-type evelvator_Info struct{
-	floor int
+
+var qeue_length int = 7
+type elevator_Info struct {
+	floor     int //where the elevator should go
+	direction int //1 up -1 down
+}
+
+type evelvator_Qeue struct {
+	floor     [qeue_length]int //the qeue of where the elevators should go
+	direction [qeue_length]int //1 up -1 down
+	age		  [qeue_length]int //the age of the call
+}
+
+func sort(curr_floor int) {
+	var new_floor	int //new floor
+	var new_direction	int //new direction
+	var scoreboard	[qeue_length]int
+
+	for i := 0; i <= qeue_length; i++{
+		if
+		//her skal algoritmen inn
+
+	}
+
 
 }
 
-func sort (){
-	if()
+func shift_Qeue() {
+
+}
+
+func pass_Qeue() {
 
 }
