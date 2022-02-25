e8f1aa9b7caef9a27c87b557855350fbb4ed3cde
Victor <victor.melhuus@gmail.com>
2022-02-25 15:57:19 +0100

    shubudubu

--- single_elevator/qeue.go
+++ single_elevator/qeue.go
@@ -1,29 +1,41 @@
 package singleElevator
 
-var qeue_length int = 7
+const queue_length int = 7
+
 type elevator_Info struct {
 	floor     int //where the elevator should go
 	direction int //1 up -1 down
 }
 
-type evelvator_Qeue struct {
-	floor     [qeue_length]int //the qeue of where the elevators should go
-	direction [qeue_length]int //1 up -1 down
-	age		  [qeue_length]int //the age of the call
+type elevator_event struct {
+	floor     int //the qeue of where the elevators should go
+	direction int //1 up -1 down
 }
 
-func sort(curr_floor int) {
-	var new_floor	int //new floor
-	var new_direction	int //new direction
-	var scoreboard	[qeue_length]int
+var elevator elevator_Info
+var queue [queue_length]elevator_event
 
-	for i := 0; i <= qeue_length; i++{
-		if
-		//her skal algoritmen inn
-
+func queue_AddEvent(new_event elevator_event) {
+	var overlap bool = false
+	for i := 0; i < queue_length; i++ {
+		if queue[i] == new_event {
+			overlap = true
+		}
 	}
+	if !overlap {
+		for i := 0; i < queue_length; i++ {
+			switch queue.direction {
+			case 1:
+				if queue[i].direction == new_event.direction {
+					if new_event.floor < queue[i].floor && new_event.floor > queue[i+1].floor {
+						queue = add_event_between(queue, new_event, i)
+					}
+				}
+			case -1:
+			}
 
-
+		}
+	}
 }
 
 func shift_Qeue() {
@@ -33,4 +45,19 @@ func shift_Qeue() {
 func pass_Qeue() {
 
 }
+func array_leftshift(in []int, length int) (out []int) {
+	for i := 0; i < length; i++ {
+		out[i] = in[i+1]
+	}
+	return out
+}
 
+func add_event_between(in [queue_length]elevator_event, new elevator_event, position int) (out [queue_length]elevator_event) {
+	for i := 0; i < position-1; i++ {
+		out[i] = in[i]
+	}
+	out[position] = new
+	for i := position + 1; i < queue_length-1; i++ {
+		out[i] = in[i+1]
+	}
+}
