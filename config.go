ae36220b6018f36518b40a94a2ba198f3eec2e0d
Victor <victor.melhuus@gmail.com>
2022-02-25 13:07:10 +0100

    Added elevator system configuration file

---
+++ config.go
@@ -0,0 +1,19 @@
+package config
+
+import "time"
+
+const (
+	ELEVATOR_ID            int    = 1
+	SIMULATION             bool   = false
+	SIMULATION_IP_AND_PORT string = ""
+	NUMBER_OF_FLOORS              = 4
+	NUMBER_OF_BUTTONS             = 3
+
+	UDP_S_R_PORT   = 20007
+	UDP_PEERS_PORT = 30007
+
+	ELEVATOR_STUCK_TIMOUT   = time.Second * 10
+	ELEVATOR_DOOR_OPEN_TIME = time.Second * 10
+
+	REMOVE_OLD_ORDER_TIME = time.Second * 2
+)
