935275ac5c6902ce8ed9b64319839f8fb8c69956
Victor <victor.melhuus@gmail.com>
2022-03-04 13:51:43 +0100

    changed the name of net to networking

--- networking/networking.go
+++ networking/networking.go
@@ -4,10 +4,18 @@ import (
 	config "PROJECT-GROUP-10/config"
 	"net"
 	"strings"
+	"time"
 )
 
-var connections [config.E]net.UDPConn
+var connections [config.NUMBER_OF_ELEVATORS]net.UDPConn
 
+type heartBeatMessage struct {
+	my_time       time.Time
+	my_direction  int
+	current_floor int
+}
+
+var myHeartBeatMessage heartBeatMessage
 var localIP string
 
 func getLocalIp() (string, error) {
@@ -23,5 +31,12 @@ func getLocalIp() (string, error) {
 }
 
 func heartBeatTransmitter() error {
+	timer := time.NewTimer(config.HEARTBEAT_TIME)
+	for {
+		select {
+		case <-timer.C:
+			myHeartBeatMessage.my_time = time.Now()
 
+		}
+	}
 }
