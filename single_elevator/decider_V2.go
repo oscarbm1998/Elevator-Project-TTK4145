1fa0b27858fc91a842096f7b5b53d4f46c41a107
Taro-tkort <joel.morlin@gmail.com>
2022-03-06 13:03:31 +0100

    kuka litt rudnt

--- single_elevator/decider_V2.go
+++ single_elevator/decider_V2.go
@@ -6,13 +6,15 @@ import (
 
 const floor_ammount int = 3
 
-type elevator_status struct {
-	floor     int
-	direction int //1 up -1 down 0 idle
-}
 type dir struct {
 	up   bool
 	down bool
+	stop bool
+}
+
+type elevator_status struct {
+	floor     int
+	direction int //1 up -1 down 0 idle
 }
 
 type floor_info struct {
@@ -28,11 +30,11 @@ var elevator_command elevator_status //where elevator should go
 func Remove_order(level int, direction int) {
 	floor[level].hall_call = 0
 	floor[level].cab_call = 0
-	if direction == 1 {
-		floor[level].direction.up = false
+	if direction == 0 {
+		floor[level].direction.up = true
 		elevio.SetButtonLamp(0, level, false)
-	} else if direction == -1 {
-		floor[level].direction.down = false
+	} else if direction == 1 {
+		floor[level].direction.down = true
 		elevio.SetButtonLamp(1, level, false)
 	}
 }
@@ -50,7 +52,6 @@ func Hall_order(
 			case 0: //opp
 				floor[a.Floor].hall_call = 1
 				floor[a.Floor].direction.up = true
-				hall_calls()
 				break
 			case 1: //ned
 				floor[a.Floor].hall_call = 1
@@ -66,38 +67,70 @@ func Hall_order(
 	}
 }
 
-//algo for utside
-func hall_calls() {
-	switch elevator.direction {
-	case 1:
-		for i := elevator.floor; i <= floor_ammount; i++ {
-			if floor[i].hall_call == 1 {
-				elevator_command.floor = i
-				break
-			}
-		}
-	case -1:
-		for i := elevator.floor; i <= 0; i-- {
-			if floor[i].hall_call == 1 {
-				elevator_command.floor = i
-				break
-			}
-		}
-	case 0:
-		for i := 0; i <= floor_ammount; i++ {
-			if floor[i].hall_call == 1 {
-				elevator_command.floor = i
-				if floor[i].hall_call > elevator.floor {
-					elevator_command.direction = 1
-				} else {
-					elevator_command.direction = -1
-				}
-				break
-			}
+func request_above() bool {
+	for i := elevator.floor; i <= floor_ammount; i++ {
+		if floor[i].hall_call == 1 {
+			elevator_command.floor = i
+			return true
 		}
 	}
+	return false
 }
 
+func request_below() bool {
+	for i := elevator.floor; i <= 0; i-- {
+		if floor[i].hall_call == 1 {
+			elevator_command.floor = i
+			return true
+		}
+	}
+	return false
+}
+
+func request_here() bool {
+	for i := 0; i <= floor_ammount; i++ {
+		if floor[i].hall_call == 1 {
+			elevator_command.floor = i
+			if floor[i].hall_call > elevator.floor {
+				elevator_command.direction = 0
+			} else {
+				elevator_command.direction = 1
+			}
+			return true
+		}
+	}
+	return false
+}
+
+func call_qeuer(direction int){
+	switch direction {
+	case 0: //up
+		if request_above(){
+		} else if request_here() {
+		} else if request_below() {
+		} else {
+			//sett den i idle
+		}
+	case 1: //down
+		if request_below(){
+		} else if request_here() {
+		} else if  request_above(){
+		} else {
+			//sett den i idle
+		}
+	case 2 //
+		if request_here(){
+		} else if request_above() {
+		} else if request_below() {
+		} else {
+			//sett den i idle
+		}
+	}
+
+
+}
+
+/*
 func Cab_calls() (found_call bool) {
 	for i := 0; i <= floor_ammount; i++ {
 		if floor[i].cab_call == 1 {
@@ -113,3 +146,4 @@ func Cab_calls() (found_call bool) {
 	}
 	return false
 }
+*/
