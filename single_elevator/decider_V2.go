8111fe87240bf5f06d47fdfd15bd246d27533a7f
Taro-tkort <joel.morlin@gmail.com>
2022-03-07 17:47:30 +0100

    did some bug fixes

--- single_elevator/decider_V2.go
+++ single_elevator/decider_V2.go
@@ -18,8 +18,8 @@ type elevator_status struct {
 }
 
 type floor_info struct {
-	up bool
-	down  bool
+	up   bool
+	down bool
 	here bool
 }
 
@@ -29,15 +29,15 @@ var elevator_command elevator_status //where elevator should go
 
 func Remove_order(level int, direction int) { //removes an order
 	floor[level].here = false //removes here call as the elevator has arrived there
-	if direction == 1 { //if the direction is up
-		floor[level].up = false //disables the up direction
+	if direction == 1 {       //if the direction is up
+		floor[level].up = false               //disables the up direction
 		elevio.SetButtonLamp(0, level, false) //turns off light
 	} else if direction == -1 { //if the direction is down
-		floor[level].down = false //disables the down direction
+		floor[level].down = false             //disables the down direction
 		elevio.SetButtonLamp(1, level, false) //turns off light
 	}
 }
-/* REVISED may not be needed
+
 func Hall_order(
 	ch_drv_buttons chan elevio.ButtonEvent,
 	ch_new_order chan bool,
@@ -48,29 +48,23 @@ func Hall_order(
 		select {
 		case a := <-ch_drv_buttons:
 			switch a.Button {
-			case 1: //opp
-				floor[a.Floor].hall_call = 1
-				floor[a.Floor].direction.up = true
-				break
-			case -1: //ned
-				floor[a.Floor].hall_call = 1
-				floor[a.Floor].direction.down = true
-				hall_calls()
-				break
-			case 0: //cab call
-				floor[a.Floor].cab_call = 1
-				Cab_calls()
+			case 0: //opp
+				floor[a.Floor].up = true
+			case 1: //ned
+				floor[a.Floor].down = true
+			case 2: //cab call
+				floor[a.Floor].here = true
 			}
 			ch_new_order <- true //forteller at en ny order er tilgjengelig
 		}
 	}
 }
-*/
+
 func request_above() bool { //checks if there are any active calls above the elevator and updates the "command struct"
 	for i := elevator.floor; i <= floor_ammount; i++ { //checks from the last known floor of the elevator to the top
-		if floor[i].up{ //if a floor with call up is found
-			elevator_command.floor = i //updates the command value
-			elevator_command.direction = 1; //sets the direction up just in case
+		if floor[i].up { //if a floor with call up is found
+			elevator_command.floor = i     //updates the command value
+			elevator_command.direction = 1 //sets the direction up just in case
 			return true
 		}
 	}
@@ -79,9 +73,9 @@ func request_above() bool { //checks if there are any active calls above the ele
 
 func request_below() bool { //checks if there are any active calls below the elevator and updates the "command struct"
 	for i := elevator.floor; i >= floor_ammount; i++ { //checks from the last known floor of the elevator to the botton
-		if floor[i].down{ //if a floor with call down is found
-			elevator_command.floor = i //updates the command value
-			elevator_command.direction = -1; //sets the direction down just in case
+		if floor[i].down { //if a floor with call down is found
+			elevator_command.floor = i      //updates the command value
+			elevator_command.direction = -1 //sets the direction down just in case
 			return true
 		}
 	}
@@ -90,9 +84,9 @@ func request_below() bool { //checks if there are any active calls below the ele
 
 func request_here() bool { //tad unshure if this is needed or not but its used for internal calls
 	for i := 0; i <= floor_ammount; i++ { //checks the entire struct for calls
-		if floor[i].here{ //if a call is found
+		if floor[i].here { //if a call is found
 			elevator_command.floor = i //update command struct
-			if floor[i].here > elevator.floor { //set direction
+			if i > elevator.floor {    //set direction
 				elevator_command.direction = 1
 			} else {
 				elevator_command.direction = -1
@@ -103,42 +97,42 @@ func request_here() bool { //tad unshure if this is needed or not but its used f
 	return false
 }
 
-func Call_qeuer(direction int) bool{
+func Call_qeuer(direction int) bool {
 	switch direction {
-	case 1 //up
-		if request_above(){
+	case 1: //up
+		if request_above() {
+			return true
 		} else if request_here() {
+			return true
 		} else if request_below() {
-		} else {
-			//sett den i idle
+			return true
 		}
-		return true
+
 	case -1: //down
-		if request_below(){
+		if request_below() {
+			return true
 		} else if request_here() {
-		} else if  request_above(){
-		} else {
-			//sett den i idle
-		}
-		return true
-	case 0 // here
-		if request_here(){
+			return true
 		} else if request_above() {
-		} else if request_below() {
-		} else {
-			//sett den i idle
+			return true
 		}
-		return true
+
+	case 0: // here
+		if request_here() {
+			return true
+		} else if request_above() {
+			return true
+		} else if request_below() {
+			return true
+		}
 	}
 	return false
- //vite når det kommer en new order
- //
 }
 
-func Update_position(int floor, int direction){
-	elevator.floor = floor
+func Update_position(level int, direction int) {
+	elevator.floor = level
 	elevator.direction = direction
-	Remove_order(floor, direction)
+	Remove_order(level, direction)
 }
 
 /*
