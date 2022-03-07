b964762ec91927925c74420163ceb5f68c698bd4
Taro-tkort <joel.morlin@gmail.com>
2022-03-07 18:40:53 +0100

    futher bugfixes

--- single_elevator/decider_V2.go
+++ single_elevator/decider_V2.go
@@ -30,10 +30,10 @@ var elevator_command elevator_status //where elevator should go
 
 func Remove_order(level int, direction int) { //removes an order
 	floor[level].here = false //removes here call as the elevator has arrived there
-	if direction == 1 {       //if the direction is up
+	if direction == 0 {       //if the direction is up
 		floor[level].up = false               //disables the up direction
 		elevio.SetButtonLamp(0, level, false) //turns off light
-	} else if direction == -1 { //if the direction is down
+	} else if direction == 1 { //if the direction is down
 		floor[level].down = false             //disables the down direction
 		elevio.SetButtonLamp(1, level, false) //turns off light
 	}
@@ -69,7 +69,7 @@ func request_above() bool { //checks if there are any active calls above the ele
 	for i := elevator.floor; i < floor_ammount; i++ { //checks from the last known floor of the elevator to the top
 		if floor[i].up { //if a floor with call up is found
 			elevator_command.floor = i     //updates the command value
-			elevator_command.direction = 1 //sets the direction up just in case
+			elevator_command.direction = 0 //sets the direction up just in case
 			return true
 		}
 	}
@@ -79,8 +79,8 @@ func request_above() bool { //checks if there are any active calls above the ele
 func request_below() bool { //checks if there are any active calls below the elevator and updates the "command struct"
 	for i := elevator.floor; i > floor_ammount; i++ { //checks from the last known floor of the elevator to the botton
 		if floor[i].down { //if a floor with call down is found
-			elevator_command.floor = i      //updates the command value
-			elevator_command.direction = -1 //sets the direction down just in case
+			elevator_command.floor = i     //updates the command value
+			elevator_command.direction = 1 //sets the direction down just in case
 			return true
 		}
 	}
@@ -92,9 +92,9 @@ func request_here() bool { //tad unshure if this is needed or not but its used f
 		if floor[i].here { //if a call is found
 			elevator_command.floor = i //update command struct
 			if i > elevator.floor {    //set direction
-				elevator_command.direction = 1
+				elevator_command.direction = 0
 			} else {
-				elevator_command.direction = -1
+				elevator_command.direction = 1
 			}
 			return true
 		}
