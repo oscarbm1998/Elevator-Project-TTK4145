35326c38ca364fbbbdc93927fd28a7db350e184e
Victor S. Melhuus <victor.melhuus@gmail.com>
2022-03-14 20:55:04 +0100

    Progress

--- networking/networking.go
+++ networking/networking.go
@@ -3,10 +3,9 @@ package networking
 import (
 	config "PROJECT-GROUP-10/config"
 	"fmt"
+	"net"
 	"strconv"
-
-	//"fmt"
-	//"strconv"
+	"strings"
 	"time"
 )
 
@@ -34,39 +33,6 @@ func Networking_main() {
 	go heartBeathandler(ch_req_ID, ch_req_data, ch_write_data)
 	go heartBeatTransmitter(ch_req_ID, ch_req_data)
 
-	/*
-				//initiate command transmit connections
-				for i := 0; i < config.NUMBER_OF_ELEVATORS-1; i++ {
-					if i != config.ELEVATOR_ID+1 {
-						network, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(config.COMMAND_SEND_PORT+i))
-						printError("Command transmit network resolve error: ", err)
-						conn, err := net.DialUDP("udp", nil, network)
-						printError("Command transmit network dial error: ", err)
-						command_cons[i] = conn
-						//Readback cons
-						network, err = net.ResolveUDPAddr("udp", ":"+strconv.Itoa(config.COMMAND_READBACK_PORT+i))
-						printError("Command transmit network resolve error: ", err)
-						conn, err = net.DialUDP("udp", nil, network)
-						printError("Command transmit network dial error: ", err)
-						readback_cons[i] = conn
-					}
-				}
-				//Initiate command readback port connection
-				adr, _ := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(config.COMMAND_READBACK_PORT+config.ELEVATOR_ID-1))
-				readback_con, _ = net.ListenUDP("udp", adr)
-
-				//Initiate command listener
-				ch_command := make(chan string)
-
-				go command_listener(ch_command)
-				//Listen for commands
-				for {
-					select {
-					case <-ch_comma
-		}
-					}
-				}
-	*/
 }
 
 //Function responsible for node data
@@ -94,47 +60,68 @@ func Node_get_data(ID int, ch_req_ID chan int, ch_req_data chan Elevator_node) (
 	return nodeData
 }
 
-/*
 func send_command(ID, floor, direction int) (success bool) {
+	//(NB!!) This function does not check if the node is alive before attempting transmission.
 	var attempts int = 1
-	var cmd string
+	var cmd, rbc, broadcast string
 	if ID == config.ELEVATOR_ID {
 		panic("Networking: I do not need networking to command myself")
 	}
 
-	cmd = strconv.Itoa(ID) + "_" + strconv.Itoa(floor) + "_" + strconv.Itoa(direction)
+	//Generate command
+	//Format: ToElevatorID_ToFloor_InDirection_FromElevatorID
+	cmd = strconv.Itoa(ID) + "_" + strconv.Itoa(floor) + "_" + strconv.Itoa(direction) + "_" + strconv.Itoa(config.ELEVATOR_ID)
+	rbc = strconv.Itoa(config.ELEVATOR_ID) + "_" + strconv.Itoa(floor) + "_" + strconv.Itoa(direction) + "_" + strconv.Itoa(ID)
+
+	//Initiate command broadcast connection
+	broadcast = "255.255.255.255:" + strconv.Itoa(config.COMMAND_PORT)
+	network, _ := net.ResolveUDPAddr("udp", broadcast)
+	cmd_con, _ := net.DialUDP("udp", nil, network)
+
+	//Initiate readback connection and timer
+	ch_rbc_msg := make(chan string)
+	ch_rbc_close := make(chan bool)
+	go command_readback_listener(ch_rbc_msg, ch_rbc_close)
 	//Send command
-	_, err := command_cons[ID-1].Write([]byte(cmd))
-	printError("Networking: Error sending command: ", err)
-	if err == nil {
-		fmt.Println("Networking: command sent to elevator " + strconv.Itoa(ID))
-	}
-	//Wait for readback
-	buf := make([]byte, 1024)
+	_, err := cmd_con.Write([]byte(cmd))
+
+	//Starting a timer for timeout
+	timOut := time.Second * 3
+	timer := time.NewTimer(timOut)
 	for {
-		n, _, err := readback_con.ReadFromUDP(buf)
-		msg := string(buf[0:n])
-
-		//Sending again if the readback is wrong
-		if msg != cmd {
-			fmt.Println("Networking: bad readback, sending again")
-			_, err = command_cons[ID-1].Write([]byte(cmd))
-			printError("Networking: Error sending command: ", err)
-			attempts++
-		} else if msg == "CMD_REJECT" { //Command rejected
-			fmt.Printf("Network: elevator rejected the command")
+		select {
+		case msg := <-ch_rbc_msg:
+			data := strings.Split(msg, "_")
+			rbc_id, _ := strconv.Atoi(data[0])
+			//Sending again if the readback is wrong
+			if rbc_id == config.ELEVATOR_ID {
+				timer.Reset(timOut)
+				if msg == rbc {
+					fmt.Println("Networking: readback OK")
+					_, err = cmd_con.Write([]byte(strconv.Itoa(ID) + "_CMD_OK"))
+					success = true
+					break
+				} else if rbc == strconv.Itoa(config.ELEVATOR_ID)+"_CMD_REJECT" { //Command rejected
+					fmt.Printf("Network: elevator rejected the command")
+					success = false
+					break
+				} else {
+					fmt.Println("Networking: bad readback, sending command again")
+					_, err = cmd_con.Write([]byte(cmd))
+					printError("Networking: Error sending command: ", err)
+					attempts++
+				}
+				if attempts > 3 {
+					fmt.Println("Networking: too many command readback attemps")
+					success = false
+					break
+				}
+			}
+		case <-timer.C:
+			fmt.Println("Networking: sending command timed out, no readback")
+			timer.Stop()
 			success = false
-			break
-		} else {
-			fmt.Println("Networking: readback OK")
-			_, err = command_cons[ID-1].Write([]byte("CMD_OK"))
-			success = true
-			break
-		}
-
-		if attempts > 2 {
-			fmt.Println("Networking: too many command readback attemps")
-			success = false
+			ch_rbc_close <- true
 			break
 		}
 	}
@@ -145,7 +132,36 @@ func send_command(ID, floor, direction int) (success bool) {
 
 	return success
 }
-*/
+
+func command_readback_listener(ch_msg chan string, ch_close chan bool) {
+	network, _ := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(config.COMMAND_RBC_PORT))
+	con, _ := net.ListenUDP("udp", network)
+	buf := make([]byte, 1024)
+	for {
+		select {
+		case <-ch_close:
+			con.Close()
+			break
+		default:
+			con.SetReadDeadline(time.Now().Add(3 * time.Second))
+			n, _, err := con.ReadFromUDP(buf)
+			if err != nil {
+				if e, ok := err.(net.Error); !ok || e.Timeout() {
+					printError("Networking: command readback net error: ", err)
+				} else {
+					fmt.Println("Networking: Getting nothing on readback channel, so quitting")
+					con.Close()
+					break
+				}
+				break
+			}
+			msg := string(buf[0:n])
+			ch_msg <- msg
+		}
+		defer con.Close()
+	}
+}
+
 /*
 func command_listener(ch_netcommand chan string) {
 	adr, _ := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(config.COMMAND_REC_PORT+config.ELEVATOR_ID-1))
