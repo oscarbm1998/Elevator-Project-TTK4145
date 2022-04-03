b2adabea4d3efc512a91835894f608eac270220d
Victor <victor.melhuus@gmail.com>
2022-04-03 19:17:29 +0200

    asdfasdf

--- networking/commanding.go
+++ networking/commanding.go
@@ -52,7 +52,7 @@ func Send_command(ID, floor, direction int) (success bool) {
 	timOut := time.Second
 	timer := time.NewTimer(timOut)
 
-	//go command_deadlockDetector(ch_deadlock_quit, 10, "Networking: sending command took too long. Possible deadlock")
+	//go command_deadlockDetector(ch_deadlock_quit, 20, "Networking: sending command took too long. Possible deadlock")
 	for {
 		select {
 		case msg := <-ch_rbc_msg:
